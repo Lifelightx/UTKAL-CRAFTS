86fc5fca75422c04258a68a8d7d4c4af0c6336d9
Jeebanjyoti Mallik <jeebanjyotimallik01@gmail.com>
2025-03-02 20:58:54 +0530

    update

--- FRONTEND/src/Components/Footer.jsx
+++ FRONTEND/src/Components/Footer.jsx
@@ -149,7 +149,7 @@ function Footer() {
                     <div className="flex flex-col md:flex-row justify-between items-center">
                         <div className="mb-4 md:mb-0">
                             <p className="text-gray-500 text-sm">
-                                &copy; {new Date().getFullYear()} Your Handcraft Store. All rights reserved.
+                                &copy; {new Date().getFullYear()} Utkal Crafts. All rights reserved.
                             </p>
                         </div>
                         <div className="flex space-x-4">
--- FRONTEND/src/Pages/Home.jsx
+++ FRONTEND/src/Pages/Home.jsx
@@ -4,8 +4,8 @@ import { StoreContext } from '../Context'
 function Home() {
     const {url} = useContext(StoreContext)
   return (
-    <div>
-      <h1>Hello world {url}</h1>
+    <div className='py-4'>
+      <h1 className='text-[#bf4221] text-4xl font-bold'>Every Thread Tells a Story, <br /> Every Craft Builds a Dream – Bringing Rural Odisha’s Heart to Your Hands</h1>
     </div>
   )
 }
